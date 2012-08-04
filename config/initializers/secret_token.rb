134b501b3dd237587fbe57b85a22268f714404e6
James Shipton <ionysis@gmail.com>
2012-08-04 11:25:36 +0100

    create rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OlympicMedals::Application.config.secret_token = '7bc30434a284b0d565adc61a2b832816eb59d6a12a1e70038a5a933e52ac4e68fdcdf1762b400baaeafef9841c0cf180f20197e9e9c4563933466fd2ff7d93a5'
