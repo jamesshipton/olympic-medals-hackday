815948c5fbbbf5965ac7ce00dcc812f777739716
James Shipton <ionysis@gmail.com>
2012-08-04 14:40:42 +0100

    Add age helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def age(dob_string)
+    dob = Date.parse(dob_string)
+    Date.today.year - dob.year - (dob.change(:year => Date.today.year) > Date.today ? 1 : 0)
+  end
 end
